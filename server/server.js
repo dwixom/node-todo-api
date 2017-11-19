783928e4f8e7729a8cf1e8a723150298cc3dd571
Dakota Wixom <dakota.wixom@gmail.com>
2017-11-19 15:55:11 -0500

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -26,6 +27,31 @@ app.get('/todos', (req, res) => {
         res.send({todos});
     }, (e) => {
         res.status(400).send(e);
+    });
+});
+
+// GET /todos/1234s
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+
+    // Valid id using isValiud
+    if(!ObjectID.isValid(id)){
+        // 404 - send back empty object
+        return res.status(404).send();
+    }
+    
+    Todo.findById(id).then((todo) => {
+        if (!todo){
+            // if no todo - send back 404 with empty body
+            return res.status(404).send();
+        }
+
+        // if todo - send it back
+        res.send({todo});
+    }).catch((e) => {
+        // error
+        // 400 - send back empty body
+        res.status(400).send();
     });
 });
 
